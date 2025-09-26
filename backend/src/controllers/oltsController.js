988a8a2dd35f0750042c8f4aa1216095171ba466
SrKlono <danilogamasilvanb@gmail.com>
2025-09-26 09:31:28 -0300

    Insert olts working

--- backend/src/controllers/oltsController.js
+++ backend/src/controllers/oltsController.js
@@ -1,5 +1,10 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import { parse } from "csv-parse";
+import path from "path";
+import fs from "fs";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -13,7 +18,7 @@ const pool = mysql.createPool({
 
 export async function getOlts(req, res) {
 	try {
-		const [rows] = await pool.query('SELECT * FROM olts');
+		const [rows] = await pool.query("SELECT * FROM olts");
 		res.status(200).json(rows);
 	} catch (error) {
 		console.error("Error in getOlts", error);
@@ -22,10 +27,28 @@ export async function getOlts(req, res) {
 }
 
 export async function insertOlt(req, res) {
-    try {
-        
-    } catch (error) {
-        console.error("Error in insertOlt", error);
+	try {
+		const olts = [];
+		const parser = fs.createReadStream(`${__dirname}/src/olts.csv`).pipe(
+			parse({
+				delimiter: [","],
+				columns: false,
+				cast: false,
+				from_line: 2
+			})
+		);
+		for await (const olt of parser) {
+			olts.push(olt);
+		}
+
+		var response = await pool.query(
+			"INSERT IGNORE INTO olts (sn, slot, port, ont_id, isonline, olt_vendor) VALUES ?",
+			[olts]
+		);
+
+		res.status(200).json(response);
+	} catch (error) {
+		console.error("Error in insertOlt", error);
 		res.status(500).json({ message: "Internal server error" });
-    }
+	}
 }
