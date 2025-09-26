5b0bec40abfcddf6d4fffdce4f71b63ba3c84e3c
SrKlono <danilogamasilvanb@gmail.com>
2025-09-26 12:04:10 -0300

    Frontend fetch working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,15 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/oltsRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/olts", router);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import api from "./lib/axios.js";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	const [olts, setOlts] = useState([]);
 
-export default App
+	useEffect(() => {
+		const fetcholts = async () => {
+			try {
+				const res = await api.get();
+				console.log(res.data);
+				setOlts(res.data);
+			} catch (error) {
+				console.error("Error fetching olt list: ", error);
+			}
+		};
+
+		fetcholts();
+	}, []);
+
+	return (
+		<div className="min-h-screen flex flex-col justify-center items-center bg-gray-800">
+
+      <h1 className="text-5xl font-bold pt-16">Virtex - ONUs</h1>
+			<table className="table-auto border-collapse m-28">
+				<thead>
+					<tr className="[&>*]:py-3 [&>*]:px-12 [&>*]:border [&>*]:border-gray-400/40 bg-gray-600 text-gray-100">
+						<th>sn</th>
+						<th>slot</th>
+						<th>port</th>
+						<th>ont_id</th>
+						<th>status</th>
+						<th>vendor</th>
+					</tr>
+				</thead>
+				<tbody>
+					{olts.length > 0 &&
+						olts.map((olt) => (
+							<tr className="[&>*]:py-3 [&>*]:px-12 [&>*]:border [&>*]:border-gray-500/40 text-center [&>*]:bg-gray-700 text-gray-300">
+								<td>{olt.sn}</td>
+								<td>{olt.slot}</td>
+								<td>{olt.port}</td>
+								<td>{olt.ont_id}</td>
+								<td className={olt.isonline ? "text-green-300": "text-red-400"}>{olt.isonline ? "online" : "offline"}</td>
+								<td>{olt.olt_vendor}</td>
+							</tr>
+						))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default App;
